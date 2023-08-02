90002c2a723d17a475ebe5450292e1457f0a076e
sayf <sayf.jawad@alliander.com>
2023-08-02 17:06:11 +0200

    another sic

--- AI-Python-Examples/Labeling.py
+++ AI-Python-Examples/Labeling.py
@@ -2,4 +2,17 @@ import numpy as np
 from sklearn import datasets
 from sklearn import preprocessing
 
+input_labels = ['red', 'black', 'red', 'green', 'black', 'yellow', 'white']
 
+encoder = preprocessing.LabelEncoder()
+encoder.fit(input_labels)
+
+print("\nLabel mappings:")
+for i, item in enumerate(encoder.classes_):
+    print(item, '-->', i)
+
+test_labels = ['green', 'red', 'black']
+encoded_values = encoder.transform(test_labels)
+
+print("\nLabels = ", test_labels)
+print("Encoded values =", list(encoded_values))
